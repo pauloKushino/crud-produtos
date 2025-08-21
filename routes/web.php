174ec69eb4a4d86dff0a9dfb6dbfe5a9fc13ec42
pauloKushino <paulokushino1@gmail.com>
2025-08-21 20:24:12 -0300

    Redireciona a rota inicial para a lista de produtos

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('produtos.index');
 });
 
 Route::resource('produtos', ProdutoController::class);
